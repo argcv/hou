a1fd583f1fadc1667e5049fdcdcbed8d06e9bf72
Yu Jing <yuikns@users.noreply.github.com>
2022-07-06 15:03:54 +0800

    build: use platform from env

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ PLATFORM="$(uname -s | tr 'A-Z' 'a-z')"
 
 export GO111MODULE=on
 export CGO_ENABLED=0
-export GOOS=linux
-#export GOOS=${PLATFORM}
+#export GOOS=linux
+export GOOS=${PLATFORM}
 go build -a -ldflags '-extldflags "-static" -s -w' ./cmd/hou
